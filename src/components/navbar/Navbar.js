12111c01ed04813f3538bea17bf9af39980afe37
v-andrusenko <andrusenko.vp1@gmail.com>
2023-04-25 00:36:20 +0300

    update

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,11 +1,26 @@
+import React, { useState } from "react";
 import support from "./../../img/icons/support.svg";
+import youtube from "./../../img/icons/youtube.png";
+import twitter from "./../../img/icons/twitter.png";
+import facebook from "./../../img/icons/facebook.png";
+import instagram from "./../../img/icons/instagram.png";
+import close from "./../../img/icons/close_button.png";
 import "./style.css";
-import Modal from "../modal/Modal.js";
 
 import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-  
+  const [modal, setModal] = useState(false);
+  const toggleModal = () => {
+    setModal(!modal);
+  };
+
+  if (modal) {
+    document.body.classList.add("active-modal");
+  } else {
+    document.body.classList.remove("active-modal");
+  }
+
   return (
     <nav className="nav-wrapper">
       <div className="container-wide">
@@ -28,14 +43,50 @@ const Navbar = () => {
             </ul>
           </div>
 
-          <a
-            href="https://www.google.com.ua/"
-            className="support"
-          >
+          <div onClick={toggleModal} className="support">
             <img src={support} alt="support" />
-          </a>
+          </div>
 
-          <Modal />
+          {modal && (
+            <div className="modal">
+              <div onClick={toggleModal}>
+                <div className="overlay">
+                  <div className="modal-content">
+                    <div class="popup_title">Контакти</div>
+                    <div class="popup_phone">+38 099 000 00 09</div>
+                    <div class="popup_email">INFO@SNB.COM</div>
+
+                    <ul className="social_media">
+                      <li>
+                        <a href="https://www.instagram.com/earth/?hl=ru">
+                          <img src={instagram} alt="instagram" className="popup_media"/>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="https://twitter.com/earth?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">
+                          <img src={twitter} alt="twitter" className="popup_media" />
+                        </a>
+                      </li>
+                      <li>
+                        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley">
+                          <img src={youtube} alt="youtube" className="popup_media" />
+                        </a>
+                      </li>
+                      <li>
+                        <a href="https://www.facebook.com/earth/">
+                          <img src={facebook} alt="facebook" className="popup_media" />
+                        </a>
+                      </li>
+                    </ul>
+
+                    <div className="popup_close" onClick={toggleModal}>
+                      <img src={close} alt="close" className="popup_media" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
 
           <button className="nav-button">
             <span className="nav-button-icon"></span>
@@ -55,10 +106,7 @@ const Navbar = () => {
                 </li>
               </ul>
             </div>
-            <a
-              href="https://www.google.com.ua/"
-              className="support"
-            >
+            <a href="https://www.google.com.ua/" className="support">
               <img src={support} alt="support" />
             </a>
           </div>
