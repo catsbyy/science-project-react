570fd1b8ab38e446d4a2cd3008540b004b0861bd
v-andrusenko <andrusenko.vp1@gmail.com>
2023-05-07 00:44:16 +0300

    update

--- src/pages/SuccessfulRegistration.js
+++ src/pages/SuccessfulRegistration.js
@@ -1,3 +1,6 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
 const SuccessfulRegistration = () => {
   return (
     <main className="landing-body">
@@ -5,10 +8,13 @@ const SuccessfulRegistration = () => {
         <article className="main-section">
           <h1 className="landing-title">Ваша анкета успішно збережена!</h1>
           <div className="main-section-wrapper">
-            <p>Роботодавець зв'яжеться з Вами, якщо Ви підійдете його вимогам</p>
-            <a href="/">
-              <button>Повернутися на головну сторінку</button>
-            </a>
+            <p>Роботодавець зв’яжеться з Вами, якщо ви підійдете його критеріям</p>
+            <NavLink to="/">
+              <button className="to-home-btn">
+                <span className="btnText">На головну</span>
+                <i className="uil uil-navigator"></i>
+              </button>
+            </NavLink>
           </div>
         </article>
       </div>
