e2b5c01076f72874246f155d978d4652e48f4c3a
v-andrusenko <andrusenko.vp1@gmail.com>
2023-05-10 13:33:10 +0300

    update

--- src/pages/SuccessfulRegistration.js
+++ src/pages/SuccessfulRegistration.js
@@ -7,17 +7,14 @@ const SuccessfulRegistration = () => {
     <main className="landing-body">
       <div className="container">
         <article className="successful-section">
-        <h1 className="landing-title">Ваша анкета успішно збережена!</h1>
-          <div className="successful-section-wrapper">
-          
-            <p className="success-text">Роботодавець зв’яжеться з Вами, якщо ви підійдете його критеріям</p>
-            <NavLink to="/">
-              <button className="to-home-btn">
-                <span className="success-btn-text">На головну</span>
-                <img className="success-img" src={home} alt="home"/>
-              </button>
-            </NavLink>
-          </div>
+          <h1 className="landing-title success-title">Вашу анкету успішно збережено!</h1>
+          <p className="success-text">Роботодавець зв’яжеться з Вами, якщо ви підійдете його критеріям</p>
+          <NavLink to="/">
+            <button className="to-home-btn">
+              <span className="success-btn-text">На головну</span>
+              <img className="success-img" src={home} alt="home" />
+            </button>
+          </NavLink>
         </article>
       </div>
     </main>
